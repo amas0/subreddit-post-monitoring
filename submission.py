c034358490d348169fda167b8b97d5c9a25071ed
amas <andrew.mascioli1@gmail.com>
2021-02-14 14:30:42 -0500

    initial commit

---
+++ submission.py
@@ -0,0 +1,77 @@
+import sqlite3
+from dataclasses import dataclass
+from datetime import datetime
+
+import praw
+
+
+@dataclass
+class SubmissionMeta:
+    id: str
+    author: str
+    created_utc: int
+    name: str
+    permalink: str
+    selftext: str
+    title: str
+
+    @classmethod
+    def from_submission(cls, sub: praw.reddit.Submission):
+        return cls(
+            id=sub.id,
+            author=sub.author.name,
+            created_utc=int(sub.created_utc),
+            name=sub.name,
+            permalink=sub.permalink,
+            selftext=sub.selftext,
+            title=sub.title
+        )
+
+    def write_to_table(self, sqlite: sqlite3.Cursor):
+        pass
+
+
+@dataclass
+class SubmissionTemporalData:
+    id: str
+    time_utc: int
+    ups: int
+    downs: int
+    num_comments: int
+
+    @classmethod
+    def from_submission(cls, sub: praw.reddit.Submission, time: int):
+        return cls(
+            id=sub.id,
+            time_utc=time,
+            ups=int(sub.ups),
+            downs=int(sub.downs),
+            num_comments=int(sub.num_comments)
+        )
+
+    def write_to_table(self, sqlite: sqlite3.Cursor):
+        pass
+
+
+def create_client(client_id: str, client_secret: str):
+    return praw.Reddit(
+        client_id=client_id,
+        client_secret=client_secret,
+        user_agent='linux:ypKsdDPvUAeMKw:v0.1.0 (by /u/_amas_)'
+    )
+
+
+def get_new_submissions(subreddit: str, client: praw.Reddit,
+                        limit: int = 10) -> tuple[list[SubmissionMeta], list[SubmissionTemporalData]]:
+    metas, temporal_data = [], []
+    for sub in client.subreddit(subreddit).new(limit=limit):
+        time_utc = int(datetime.utcnow().timestamp())
+        metas.append(SubmissionMeta.from_submission(sub))
+        temporal_data.append(SubmissionTemporalData.from_submission(sub, time_utc))
+    return metas, temporal_data
+
+
+def get_temp_data_from_id(submission_id: str, client: praw.Reddit) -> SubmissionTemporalData:
+    sub = client.submission(submission_id)
+    time_utc = int(datetime.utcnow().timestamp())
+    return SubmissionTemporalData.from_submission(sub, time_utc)
